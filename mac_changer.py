ef3981703f832865cae12d3c0da56a87bc1e9bbe
Aldin Selic <52328155+themozak@users.noreply.github.com>
2019-11-02 21:20:59 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
+import re
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:00:00:00:00:00", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+def get_arguments():
+	parser = optparse.OptionParser() 									
+	parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+	parser.add_option("-m", "--mac", dest="new_mac_addr", help="New MAC address") 				
+	(options, arguments) = parser.parse_args()
+	if not options.interface:
+		parser.error("[-] Specify interface or use --help for help")
+	elif not options.new_mac_addr:
+		parser.error("[-] Specify MAC addr or use --help for help")
+	return options
+
+def mac_change(interface, new_mac_addr):
+	print("[+] Changing MAC address for " + interface + " to " + new_mac_addr)
+	subprocess.call(["ifconfig", interface, "down"])
+	subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_addr])
+	subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+	ifconfig_result = subprocess.check_output(["ifconfig", interface])
+	mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+	if mac_address_search_result:
+		return mac_address_search_result.group(0)
+	else:
+		print("Could not read MAC address.")
+
+options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("[+] Current MAC = " + str(current_mac))
+
+mac_change(options.interface, options.new_mac_addr)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac_addr:
+	print("[+] MAC address was successfully changed to " + current_mac)
+else:
+	print("[-] MAC address did not change.")
